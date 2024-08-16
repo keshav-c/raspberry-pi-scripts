c5d8483b236a8df2d440fc625019d0e1ee17f3d7
Keshav Chakravarthy <keshav@pifive1.com>
2024-08-16 16:44:02 +0530

    Permission changes

--- pi-temps.sh
+++ pi-temps.sh
@@ -5,6 +5,6 @@
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 echo "$(hostname) @ $(date)"
 echo "-----------------TEMP------------------"
-echo "GPU: $(vcgencmd measure_temp)"
+echo "GPU: $(vcgencmd measure_temp | grep  -o -E '[[:digit:]].*')"
 echo "CPU: $((cpu/1000))'C"
 
